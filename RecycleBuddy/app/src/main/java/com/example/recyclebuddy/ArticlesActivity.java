d5e011f5daedd449ca609faf7770f2441e3b5d62
cnmeier <31703745+cnmeier@users.noreply.github.com>
2020-02-01 12:30:31 -0500

    add tabs to articles page

--- RecycleBuddy/app/src/main/java/com/example/recyclebuddy/ArticlesActivity.java
+++ RecycleBuddy/app/src/main/java/com/example/recyclebuddy/ArticlesActivity.java
@@ -1,6 +1,8 @@
 package com.example.recyclebuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,53 @@ public class ArticlesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_articles);
+    }
+
+    /**
+     * Sets up the screen that follows after the user clicks the store button
+     * on the search screen
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goSearch(View v) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        this.startActivity(intent);
+    }
+
+    /**
+     * Sets up the screen that follows after the user clicks the articles button
+     * on the search screen
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goNews(View v) {
+        Intent intent = new Intent(this, ArticlesActivity.class);
+        this.startActivity(intent);
+    }
+
+    /**
+     * Sets up the screen that follows after the user clicks the store button
+     * on the search screen
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goStore(View v) {
+        Intent intent = new Intent(this, StoreActivity.class);
+        this.startActivity(intent);
+    }
+
+    /**
+     * Sets up the screen that follows after the user clicks the profile button
+     * on the search screen
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goProfile(View v) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        this.startActivity(intent);
     }
 }
