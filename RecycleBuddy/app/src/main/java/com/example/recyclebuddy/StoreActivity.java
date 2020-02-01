6f3384cbc0c74fb50ac09af666784e33e34ec5f4
cnmeier <31703745+cnmeier@users.noreply.github.com>
2020-02-01 01:43:56 -0500

    add store activity

---
+++ RecycleBuddy/app/src/main/java/com/example/recyclebuddy/StoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.recyclebuddy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store);
+    }
+}
