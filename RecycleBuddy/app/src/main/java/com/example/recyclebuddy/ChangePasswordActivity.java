99ca19a5bc165898428b7285b8a37826ea0fecae
cnmeier <31703745+cnmeier@users.noreply.github.com>
2020-02-01 11:54:57 -0500

    add change password option

--- RecycleBuddy/app/src/main/java/com/example/recyclebuddy/ChangePasswordActivity.java
+++ RecycleBuddy/app/src/main/java/com/example/recyclebuddy/ChangePasswordActivity.java
@@ -1,4 +1,53 @@
 package com.example.recyclebuddy;
 
-public class ChangePasswordActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    /**
+     * Creates the screen of the app for where the user changes their password and
+     * displays a dialog box that states whether it worked or not
+     *
+     * @param savedInstanceState
+     *            the previous state of the application
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changepassword);
+    }
+
+    /**
+     * Opens the app on the start
+     */
+    public void onStart() {
+        super.onStart();
+    }
+
+    /**
+     * Sets up the screen that allows the user to change their password on their
+     * profile page
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goProfile(View v) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        this.startActivity(intent);
+    }
+
+    /**
+     * Sets up the screen that goes to the settings page
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goSettings(View v) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        this.startActivity(intent);
+    }
 }
