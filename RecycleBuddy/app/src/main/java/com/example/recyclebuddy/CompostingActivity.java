caceb741e4c8bd028e7743f83dc0f05011ebe18b
cnmeier <31703745+cnmeier@users.noreply.github.com>
2020-02-01 15:05:03 -0500

    add functionality to tips page

---
+++ RecycleBuddy/app/src/main/java/com/example/recyclebuddy/CompostingActivity.java
@@ -0,0 +1,14 @@
+package com.example.recyclebuddy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CompostingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_composting);
+    }
+}
