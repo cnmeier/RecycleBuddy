0a07de1ca922f427b6abba5e489544001f39ecf8
Patrick Sacchet <pjsacchet@loyola.edu>
2020-02-01 12:38:32 -0500

    Initial MongoDB connection

--- RecycleBuddy/app/src/main/java/com/example/recyclebuddy/MainActivity.java
+++ RecycleBuddy/app/src/main/java/com/example/recyclebuddy/MainActivity.java
@@ -1,10 +1,30 @@
 package com.example.recyclebuddy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.mongodb.stitch.android.core.Stitch;
+import com.mongodb.stitch.android.core.StitchAppClient;
+import com.mongodb.stitch.android.core.auth.StitchUser;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
+import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential;
+import com.mongodb.stitch.core.services.mongodb.remote.sync.ChangeEventListener;
+import com.mongodb.stitch.core.services.mongodb.remote.sync.DefaultSyncConflictResolvers;
+import com.mongodb.stitch.core.services.mongodb.remote.sync.ErrorListener;
+import com.mongodb.stitch.core.services.mongodb.remote.sync.internal.ChangeEvent;
+
+import org.bson.BsonValue;
+import org.bson.Document;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +32,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+    RemoteMongoCollection _remoteCollection;
+
+
+
+    public void connectMongo() {
+        //RemoteMongoCollection _remoteCollection;
+
+
+        // Create the StitchAppClient
+        final StitchAppClient client = Stitch.initializeAppClient("<APP_ID>");
+
+
+        // Log-in using an Anonymous authentication provider from Stitch
+        client.getAuth().loginWithCredential(new AnonymousCredential())
+                .addOnCompleteListener(new OnCompleteListener<StitchUser>() {
+                    @Override
+                    public void onComplete(@NonNull Task<StitchUser> task) {
+                        // Get a remote client
+                        final RemoteMongoClient remoteMongoClient =
+                                client.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+
+                        // Set up the atlas collection
+                         _remoteCollection = remoteMongoClient
+                                .getDatabase("my_db").getCollection("my_collection");
+                        _remoteCollection.sync().configure(
+                                DefaultSyncConflictResolvers.remoteWins(),
+                                new MyUpdateListener(),
+                                new MyErrorListener());
+                    }
+                });
+    }
+
+
 
     /**
      * Sets up the screen that follows after the user clicks the signup button
@@ -37,4 +94,32 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SearchActivity.class);
         this.startActivity(intent);
     }
+
+
+    private class MyErrorListener implements ErrorListener {
+        @Override
+        public void onError(BsonValue documentId, Exception error) {
+            Log.e("Stitch", error.getLocalizedMessage());
+            Set<BsonValue> docsThatNeedToBeFixed = _remoteCollection.sync().getPausedDocumentIds();
+            for (BsonValue doc_id : docsThatNeedToBeFixed) {
+                // Add your logic to inform the user.
+                // When errors have been resolved, call
+                _remoteCollection.sync().resumeSyncForDocument(doc_id);
+            }
+            // refresh the app view, etc.
+        }
+    }
+
+    private class MyUpdateListener implements ChangeEventListener<Document> {
+        @Override
+        public void onEvent(final BsonValue documentId, final ChangeEvent<Document> event) {
+            if (!event.hasUncommittedWrites()) {
+                // Custom actions can go here
+            }
+            // refresh the app view, etc.
+        }
+    }
+
+
+
 }
