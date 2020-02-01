8998175ec34722af535d0e391ee3ce8f6ae638ef
cnmeier <31703745+cnmeier@users.noreply.github.com>
2020-01-31 22:39:02 -0500

    go to signup page works

--- RecycleBuddy/app/src/main/java/com/example/recyclebuddy/MainActivity.java
+++ RecycleBuddy/app/src/main/java/com/example/recyclebuddy/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.recyclebuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Sets up the screen that follows after the user clicks the signup button
+     * on the first screen
+     *
+     * @param v
+     *            the screen view
+     */
+    public void gosignup(View v) {
+        Intent intent = new Intent(this, SignupActivity.class);
+        this.startActivity(intent);
     }
 }
