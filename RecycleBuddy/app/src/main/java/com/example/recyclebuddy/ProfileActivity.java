3cc58c5ad27ad550813a69c1dba0a77ddef985e9
cnmeier <31703745+cnmeier@users.noreply.github.com>
2020-02-01 12:22:10 -0500

    add tabs to other pages

--- RecycleBuddy/app/src/main/java/com/example/recyclebuddy/ProfileActivity.java
+++ RecycleBuddy/app/src/main/java/com/example/recyclebuddy/ProfileActivity.java
@@ -26,4 +26,52 @@ public class ProfileActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SettingsActivity.class);
         this.startActivity(intent);
     }
+
+    /**
+     * Sets up the screen that follows after the user clicks the store button
+     * on the search screen
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goSearch(View v) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        this.startActivity(intent);
+    }
+
+    /**
+     * Sets up the screen that follows after the user clicks the articles button
+     * on the search screen
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goNews(View v) {
+        Intent intent = new Intent(this, ArticlesActivity.class);
+        this.startActivity(intent);
+    }
+
+    /**
+     * Sets up the screen that follows after the user clicks the store button
+     * on the search screen
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goStore(View v) {
+        Intent intent = new Intent(this, StoreActivity.class);
+        this.startActivity(intent);
+    }
+
+    /**
+     * Sets up the screen that follows after the user clicks the profile button
+     * on the search screen
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goProfile(View v) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        this.startActivity(intent);
+    }
 }
