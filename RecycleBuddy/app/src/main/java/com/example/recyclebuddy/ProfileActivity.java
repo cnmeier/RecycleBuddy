6982ff3540cfa032f9dab2d4fcdb08ad512f6041
cnmeier <31703745+cnmeier@users.noreply.github.com>
2020-02-01 12:11:25 -0500

    design profile page

--- RecycleBuddy/app/src/main/java/com/example/recyclebuddy/ProfileActivity.java
+++ RecycleBuddy/app/src/main/java/com/example/recyclebuddy/ProfileActivity.java
@@ -1,6 +1,10 @@
 package com.example.recyclebuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Set;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,16 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    /**
+     * Sets up the screen that follows after the user clicks the settings button
+     *
+     * @param v
+     *            the screen view
+     */
+    public void goSettings(View v) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        this.startActivity(intent);
     }
 }
