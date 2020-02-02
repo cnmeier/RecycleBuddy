80c901ef768294c78e4a855631333bf5002032bf
cnmeier <31703745+cnmeier@users.noreply.github.com>
2020-02-01 20:56:34 -0500

    attempt to get logout button to work

--- RecycleBuddy/app/src/main/java/com/example/recyclebuddy/SettingsActivity.java
+++ RecycleBuddy/app/src/main/java/com/example/recyclebuddy/SettingsActivity.java
@@ -2,7 +2,9 @@ package com.example.recyclebuddy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +20,18 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button logout = (Button) findViewById(R.id.logout);
+
+        logout.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+
+                        goLogin(null);
+                    }
+                });
     }
 
     /**
